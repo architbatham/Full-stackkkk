ea6e89130064c10a974b186b0050f7358b48da85
archit batham <architbatham64@gmail.com>
2024-07-01 22:24:52 +0530

    done

--- Project/myapp/src/Component/Twelve.js
+++ Project/myapp/src/Component/Twelve.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Twelve() {
+    const [file,setFile] = useState(null);
+
+    const handleFileChange = (e) => {
+        const selectedFile = e.target.files[0];
+        setFile(selectedFile)
+    }
+
   return (
     <div>
-        <input type='file' accept='image/*'/>
+        <input type='file' accept='image/*' onChange={handleFileChange} />
+        {file && <img src={URL.createObjectURL(file)} height={200}  alt='Uploaded' />}
     </div>
   )
 }
