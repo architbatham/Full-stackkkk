6135d48d61475648f97d790ecc0544b1bd47f5e9
archit batham <architbatham64@gmail.com>
2024-07-08 18:06:46 +0530

    done

---
+++ Frontend/Functions/Default Function.js
@@ -0,0 +1,25 @@
+//Default function
+console.log("Before");
+
+
+
+function myapp(a,b){
+
+    // var a,b,c;
+    // a=10;
+    // b=20;
+    c=a+b;
+    console.log("Sum of a and b is = "+  c);
+}
+
+ var a,b,c;
+    a=10;
+    b=20;
+    myapp(a,b);
+
+
+
+
+console.log("After");
+
+//Function without argument without returnn
